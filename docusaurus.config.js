e558e500d5a51eb5bda01561a14000cfdd7dc926
root <root@rebelmythik.vps>
2023-03-30 18:57:07 -0400

    initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'LegalSmeagol', // Usually your GitHub org/user name.
-  projectName: 'havensmp-wiki/tree/main/docs',
+  projectName: 'havensmp-wiki',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
