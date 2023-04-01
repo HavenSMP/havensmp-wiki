b180dcdc63af7282991fa65bdb1d33fd93dec8c6
LegalSmeagol <49310887+LegalSmeagol@users.noreply.github.com>
2023-04-01 14:35:22 +0100

    Delete assets directory

--- assets/js/f0110cee.2cf50e92.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkhavensmp_wiki=self.webpackChunkhavensmp_wiki||[]).push([[605],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
